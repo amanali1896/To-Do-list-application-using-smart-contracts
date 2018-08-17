a7391587ff247ebd238cbf8f5f8d3333408f5e12
Aman Ali <pamanalionline@gmail.com>
2018-08-18 04:37:08 +0530

    Update TodoList.sol

--- todo-dapp/TodoList.sol
+++ todo-dapp/TodoList.sol
@@ -1,8 +1,10 @@
-pragma solidity 0.4.20;
-contract TodoList {
+pragma solidity 0.4.20; // declaring the compiler version
+contract TodoList { // declaring the contract
     struct Todo{
-        bytes32 content;
-        
+        bytes32 content; // contents of the list
+        address owner; //owner's address
+        bool isCompleted;// marking if the owner has completed the list or not
+        uint256 timestamp;//timestamp of the document
     }
     
 }
