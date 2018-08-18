935c53cfb8f4d53e09d1bc991581c8519a5dcbd0
Aman Ali <pamanalionline@gmail.com>
2018-08-18 12:16:22 +0530

    Update TodoList.sol

--- todo-dapp/TodoList.sol
+++ todo-dapp/TodoList.sol
@@ -8,9 +8,7 @@ contract TodoList { // declaring the contract
         uint256 timestamp;//timestamp of the document/note being created.
     }
     uint256 public constant maxAmountOfTodos = 100; /*This variable limits the amount of lists*/ 
-    // Owner => todos
     mapping(address => Todo[maxAmountOfTodos]) public todos;// address of one user is mapped to all the notes created by user.
-    // Owner => last todo id
     mapping(address => uint256) public lastIds;/* we store the last IDs of each user required to create new notes.
                                                 We do this since to add a new note we need to have track of the last used note id*/
 }
