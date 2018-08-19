694544af6efe18127cd1e0519f7a0e3a8994c889
Aman Ali <pamanalionline@gmail.com>
2018-08-19 15:00:42 +0530

    Update TodoList.sol

--- todo-dapp/TodoList.sol
+++ todo-dapp/TodoList.sol
@@ -8,7 +8,30 @@ contract TodoList { // declaring the contract
         uint256 timestamp;//timestamp of the document/note being created.
     }
     uint256 public constant maxAmountOfTodos = 100; /*This variable limits the amount of lists*/ 
-    mapping(address => Todo[maxAmountOfTodos]) public todos;// address of one user is mapped to all the notes created by user.
+    mapping(address => Todo[maxAmountOfTodos]) public todos;/* address of one user is mapped 
+                                                                to all the notes created by user.*/
     mapping(address => uint256) public lastIds;/* we store the last IDs of each user required to create new notes.
-                                                We do this since to add a new note we need to have track of the last used note id*/
+                             We do this since to add a new note we need to have track of the last used note id*/
+    modifier onlyOwner(address _owner) { /* this is done so that only the real owner of the note can access 
+                                        the note and moify its contents*/ 
+        require(msg.sender == _owner); /*only if this condition is true then
+                                        execute the function else throw an exception*/
+        _;
+    }
+    // Add a todo to the list
+    function addTodo(bytes32 _content) public { /* this is declared to create the 
+                                                contents */
+
+        Todo memory myNote = Todo(lastIds[msg.sender], // creating the note by adding the values in its constructor
+            _content, msg.sender, false, now); /** “memory”,  is used to hold temporary values. */
+        todos[msg.sender][lastIds[msg.sender]] = myNote; /** I am now adding the created note to the mapping. 
+                                                            which means that we add the note to the already existing 
+                                                            notes by the user  */
+        if (lastIds[msg.sender] >= maxAmountOfTodos) //last ID is updated so that we have a track of the note numbers. 
+                                                    // is restricted to 100
+            lastIds[msg.sender] = 0; //if it overflows
+                                        
+        else 
+            lastIds[msg.sender]++; //incrementing it by one
+    }
 }
